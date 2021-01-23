9beb340c20620c7f7bf5d1b057799fc1d5bddbb6
Laurent Michel <laurent.michel@softozor.ch>
2021-01-23 18:47:04 +0100

    hello world example

---
+++ HelloWorld/Producer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var factory = new ConnectionFactory
+      {
+        HostName = "localhost",
+        UserName = "admin",
+        Password = "password"
+      };
+      using (var connection = factory.CreateConnection())
+      {
+        using (var channel = connection.CreateModel())
+        {
+          channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+          var message = "Hello World!";
+          var body = Encoding.UTF8.GetBytes(message);
+
+          channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+          Console.WriteLine("Sent {0}", message);
+        }
+      }
+
+      Console.WriteLine("Press [enter] to exit");
+      Console.ReadLine();
+    }
+  }
+}
